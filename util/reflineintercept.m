5f66f8c407050c5a4dbf91cf8508c23ca1c721d8
mgcooper <mguycooper@gmail.com>
2022-07-11 12:40:01 -0700

    Update reflineintercept.m

--- util/reflineintercept.m
+++ util/reflineintercept.m
@@ -17,12 +17,8 @@ function intercept = reflineintercept(x,y,slope,refpoint,modeltype,xyref)
          case 'power'
             intercept = refpoint/x(findmin(abs(y-refpoint),1,'first'))^slope;
 
-            % might be better to use this, but it doesn't work if mask
-            %intercept = yrefpoint/mean(x.^slope);
-
          case 'linear'
             intercept = refpoint-x(findmin(abs(y-refpoint),1,'first'))*slope;
-           %intercept = yrefpoint/x(findmin(abs(y-yrefpoint),1,'first'))/slope;
       end   
       
       case 'x'
@@ -31,9 +27,6 @@ function intercept = reflineintercept(x,y,slope,refpoint,modeltype,xyref)
 
          case 'power'
             intercept = y(findmin(abs(y-refpoint),1,'first'))/refpoint^slope;
-
-            % might be better to use this, but it doesn't work if mask
-            % intercept = yrefpoint/mean(x.^slope);
 
          case 'linear'
             intercept = y(findmin(abs(y-refpoint),1,'first'))-refpoint*slope;
