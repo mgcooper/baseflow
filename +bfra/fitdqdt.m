6e13442aaf7bf7b44569adb11c84a5814299a179
mgcooper <mguycooper@gmail.com>
2022-11-06 12:11:37 -0800

    added documentation

--- +bfra/fitdqdt.m
+++ +bfra/fitdqdt.m
@@ -1,5 +1,28 @@
 function [q,dqdt,dt,tq,rq,r] = fitdqdt(T,Q,R,method,varargin)
 %FITDQDT estimate q and dqdt during recession events to send to fitab
+% 
+%  Syntax
+%     [q,dqdt,dt,tq,rq,r] = bfra.fitdqdt(T,Q,R,derivmethod)
+%     [q,dqdt,dt,tq,rq,r] = bfra.fitdqdt(_,'fitwindow',fitwindow)
+%     [q,dqdt,dt,tq,rq,r] = bfra.fitdqdt(_,'fitwindow',fitmethod)
+%     [q,dqdt,dt,tq,rq,r] = bfra.fitdqdt(_,'pickmethod',pickmethod)
+%     [q,dqdt,dt,tq,rq,r] = bfra.fitdqdt(_,'ax',axis_object)
+% 
+%  Required inputs
+%     T     =  time (days)
+%     Q     =  discharge (L T^-1, assumed to be m d-1 or m^3 d-1)
+%     R     =  rainfall (L T^-1, assumed to be mm d-1)
+%     derivmethod = method to compute numerical derivative dQ/dt. Options are
+%     'VTS','ETS','B1','B2','F1','F2','C2','C4','SGO','SPN','SLM'. default: ETS
+% 
+%  Optional name-value pairs
+% 
+%     etsparam =  scalar double, parameter that controls window size in ETS method
+%     vtsparam =  scalar double, parameter that controls window size in VTS method
+%     fitab    =  logical, scalar, indicates whether to fit a/b in -dQ/dt=aQb
+%     plotfit  =  logical, scalar, indicates whether to plot the fit
+% 
+%  See also getdqdt
 
 % input parsing
 %-------------------------------------------------------------------------------
@@ -73,7 +96,7 @@ plotfit = p.Results.plotfit; % otherwise builtin plotfit is called
    elseif strcmp(method,'ETS')
       
       % this assumes the event t,q are passed in
-      Fit     =   bfra.fitETS(T,Q,R,'nsteps',etsparam,'fitab',fitab,'plot',plotfit);
+      Fit     =   bfra.fitets(T,Q,R,'nsteps',etsparam,'fitab',fitab,'plot',plotfit);
       q       =   Fit.T.q;
       dq      =   Fit.T.dq;
       dt      =   Fit.T.dt;
@@ -87,7 +110,7 @@ plotfit = p.Results.plotfit; % otherwise builtin plotfit is called
    elseif any(strcmp(method,{'B1','B2','F1','F2','C2','C4'}))
       
       % NOTE: to use any of these that involve more than 1 timestep
-      % forward or backward, I'll need to adjust getevents to return a
+      % forward or backward, I'll need to adjust findevents to return a
       % longer timeseries
       
       % offset vectors to compute derivatives
