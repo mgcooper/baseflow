b92462567697348023a9f2ccbad3039f697dfc21
mgcooper <mguycooper@gmail.com>
2022-11-07 20:14:41 -0800

    removed Meta from input and added all supported input parser options

--- +bfra/globalfit.m
+++ +bfra/globalfit.m
@@ -1,11 +1,12 @@
-function GlobalFit = globalfit(K,Events,Fits,Meta,varargin)
+function GlobalFit = globalfit(K,Events,Fits,varargin)
 %GLOBALFIT takes the event-scale recession analysis parameters saved in
 %data table K and the event-scale data saved in Events and Fits and computes
 %'global' parameters tau, tau0, phi, bhat, ahat, Qexp, and Q0
 % 
 % Syntax:
 % 
-%  FIT = bfra.GLOBALFIT(K,Events,Fits,Meta);
+%  FIT = bfra.GLOBALFIT(K,Events,Fits);
+%  FIT = bfra.GLOBALFIT(K,Events,Fits,opts);
 %  FIT = bfra.GLOBALFIT(K,Events,Fits,Meta,'plotfits',plotfits);
 %  FIT = bfra.GLOBALFIT(K,Events,Fits,Meta,'bootfit',bootfit);
 %  FIT = bfra.GLOBALFIT(K,Events,Fits,Meta,'bootfit',bootfit,'nreps',nreps);
@@ -15,11 +16,13 @@ function GlobalFit = globalfit(K,Events,Fits,Meta,varargin)
 
 % Required inputs:
 %  K, Events, Fits - output of bfra.getevents and bfra.dqdt
-%  Meta - struct containing fields area, D0, and L (see below)
+%  opts - struct containing fields area, D0, and L (see below)
 %  AnnualFlow - timetable or table of annual flow containing field Qcmd which
 %  is the average daily flow (units cm/day) posted annually. 
 %  TODO: make the inputs more general, rather than these hard-coded structures
 %  and tables
+%
+% See also setopts
 
 %-------------------------------------------------------------------------------
 % input parsing
@@ -27,32 +30,44 @@ function GlobalFit = globalfit(K,Events,Fits,Meta,varargin)
 p                 = inputParser;
 p.FunctionName    = 'bfra.globalfit';
 p.PartialMatching = true;
-addRequired(p,    'K',                       @(x)istable(x)             );
-addRequired(p,    'Events',                  @(x)isstruct(x)            );
-addRequired(p,    'Fits',                    @(x)isstruct(x)            );
-addRequired(p,    'Meta',                    @(x)isstruct(x)|istable(x) );
-addParameter(p,   'plotfits', false,         @(x)islogical(x)           );
-addParameter(p,   'bootfit',  false,         @(x)islogical(x)           );
-addParameter(p,   'nreps',    1000,          @(x)isnumeric(x)           );
-addParameter(p,   'phimethod','pointcloud',  @(x)ischar(x)              );
-addParameter(p,   'refqtls',  [0.50 0.50],   @(x)isnumeric(x)           );
+p.StructExpand    = true;
+addRequired(p,    'K',                             @(x)istable(x)                );
+addRequired(p,    'Events',                        @(x)isstruct(x)               );
+addRequired(p,    'Fits',                          @(x)isstruct(x)               );
+addParameter(p,   'drainagearea',   nan,           @(x)isnumericscalar(x)        );
+addParameter(p,   'aquiferdepth',   nan,           @(x)isnumericscalar(x)        );
+addParameter(p,   'streamlength',   nan,           @(x)isnumericscalar(x)        );
+addParameter(p,   'aquiferslope',   nan,           @(x)isnumericscalar(x)        );
+addParameter(p,   'aquiferbreadth', nan,           @(x)isnumericscalar(x)        );
+addParameter(p,   'isflat',         true,          @(x)islogicalscalar(x)        );
+addParameter(p,   'plotfits',       false,         @(x)islogicalscalar(x)        );
+addParameter(p,   'bootfit',        false,         @(x)islogicalscalar(x)        );
+addParameter(p,   'nreps',          1000,          @(x)isdoublescalar(x)         );
+addParameter(p,   'phimethod',      'pointcloud',  @(x)ischar(x)                 );
+addParameter(p,   'refqtls',        [0.50 0.50],   @(x)isnumericvector(x)        );
+addParameter(p,   'earlyqtls',      [0.90 0.90],   @(x)isnumericvector(x)        );
+addParameter(p,   'lateqtls',       [0.50 0.50],   @(x)isnumericvector(x)        );
+      
 
-parse(p,K,Events,Fits,Meta,varargin{:});
+parse(p,K,Events,Fits,varargin{:});
 
-plotfits    = p.Results.plotfits;
-bootfit     = p.Results.bootfit;
-nreps       = p.Results.nreps;
-phimethod   = p.Results.phimethod;
-refqtls     = p.Results.refqtls;
+drainagearea   = p.Results.drainagearea;     % basin area [m2]
+aquiferdepth   = p.Results.aquiferdepth;     % reference active layer thickness [m]
+streamlength   = p.Results.streamlength;     % effective stream network length [m]
+aquiferslope   = p.Results.aquiferslope;
+aquiferbreadth = p.Results.aquiferbreadth;
+plotfits       = p.Results.plotfits;
+bootfit        = p.Results.bootfit;
+nreps          = p.Results.nreps;
+phimethod      = p.Results.phimethod;
+refqtls        = p.Results.refqtls;
+earlyqtls      = p.Results.earlyqtls;
+lateqtls       = p.Results.lateqtls;
 
 %-------------------------------------------------------------------------------
 
 % take values out of the data structures that are needed
 Q  = Events.Q;       % daily streamflow [m3 d-1]
-A  = Meta.A;         % basin area [m2]
-D  = Meta.D;         % reference active layer thickness [m]
-L  = Meta.L;         % effective stream network length [m]
-tf = Meta.isflat;    % use the horizontal or sloped aquifer solution 
 
 % fit tau, a, b (tau [days], q [m3 d-1], dqdt [m3 d-2])
 %---------------
@@ -83,16 +98,19 @@ itau     = TauFit.taumask;
 %---------
 switch phimethod 
    case 'distfit'
-      phid = bfra.eventphi(K,Fits,A,D,L,bhat,'refqtls',refqtls);
+      phid = bfra.eventphi(K,Fits,drainagearea,aquiferdepth,streamlength,bhat,...
+                           'refqtls',refqtls);
       phi = bfra.fitdistphi(phid,'mu','cdf');
    case 'pointcloud'
-      phi = bfra.cloudphi(q,dqdt,bhat,A,D,L,'envelope','refqtls',refqtls,'mask',itau);
+      phi = bfra.cloudphi(q,dqdt,bhat,drainagearea,aquiferdepth,streamlength,...
+                           'envelope','refqtls',refqtls,'mask',itau);
 end
 
 
 % fit k
 %---------
-[k,Q0_2,D_2] = bfra.aquiferprops(q,dqdt,ahat,bhat,phi,A,D,L,'RS05','mask',itau);
+[k,Q0_2,D_2] = bfra.aquiferprops(q,dqdt,ahat,bhat,phi,drainagearea,...
+                  aquiferdepth,streamlength,'RS05','mask',itau);
 
 
 % Q0    = Qexp*(3-b)/(2-b);
