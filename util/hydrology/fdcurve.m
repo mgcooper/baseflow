293eac37227f8f2185f07d5a9254c302ef660b41
mgcooper <mguycooper@gmail.com>
2022-12-03 12:30:31 -0800

    removed end from functions

--- util/hydrology/fdcurve.m
+++ util/hydrology/fdcurve.m
@@ -2,77 +2,79 @@ function fdc = fdcurve(flow,varargin)
 %FDCURVE Flow duration curve
 
 %-------------------------------------------------------------------------------
-   p = MipInputParser;
-   p.FunctionName='fdcurve';
-   p.addRequired('flow',@(x)isnumeric(x));
-   p.addParameter('axscale','semilogy',@(x)ischar(x));
-   p.addParameter('units','',@(x)ischar(x));
-   p.addParameter('refpoints',nan,@(x)isnumeric(x));
-   p.addParameter('plotcurve',true,@(x)islogical(x));
-   p.parseMagically('caller');
+p              = MipInputParser;
+p.FunctionName = 'fdcurve';
+
+p.addRequired( 'flow',                    @(x)isnumeric(x)  );
+p.addParameter('axscale',     'semilogy', @(x)ischar(x)     );
+p.addParameter('units',       '',         @(x)ischar(x)     );
+p.addParameter('refpoints',   nan,        @(x)isnumeric(x)  );
+p.addParameter('plotcurve',   true,       @(x)islogical(x)  );
+
+p.parseMagically('caller');
 %-------------------------------------------------------------------------------
    
-   N     = length(flow);
-   M     = 1:N;
-   x     = sort(flow,'descend');
-   f     = 1-M./(N+1);
-   
-   % if requested, compute reference point values
-   xref = nan(numel(refpoints,1));
-   fref = nan(numel(refpoints,1));
+N     = length(flow);
+M     = 1:N;
+x     = sort(flow,'descend');
+f     = 1-M./(N+1);
+
+% if requested, compute reference point values
+xref  = nan(numel(refpoints,1));
+fref  = nan(numel(refpoints,1));
+if ~isnan(refpoints)
+   for n = 1:numel(refpoints)
+      iref     = find(x>=refpoints(n),1,'last');
+      xref(n)  = x(iref);
+      fref(n)  = f(iref);
+   end
+end
+
+% plot the curve if requested
+if plotcurve == true
+   figure;
+   switch axscale
+      case 'loglog'
+         h.fdc = loglog(100.*f,x); ax = gca;
+      case 'semilogy'
+         h.fdc = semilogy(100.*f,x); ax = gca;
+      case 'semilogx'
+         h.fdc = semilogx(100.*f,x); ax = gca;
+      case 'linear'
+         h.fdc = plot(100.*f,x); ax = gca;
+   end
+
+   % if requested, add a refpoint line
    if ~isnan(refpoints)
+      hold on;
       for n = 1:numel(refpoints)
-         iref     = find(x>=refpoints(n),1,'last');
-         xref(n)  = x(iref);
-         fref(n)  = f(iref);
+         xplot    = [min(xlim) 100*fref(n) 100*fref(n) 100*fref(n)];
+         yplot    = [xref(n) xref(n) min(ylim) xref(n)];
+         h.ref(n) = plot(xplot,yplot,'Color',[0.85 0.325 0.098],'LineWidth',1);
       end
    end
 
-   % plot the curve if requested
-   if plotcurve == true
-      figure;
-      switch axscale
-         case 'loglog'
-            h.fdc = loglog(100.*f,x); ax = gca;
-         case 'semilogy'
-            h.fdc = semilogy(100.*f,x); ax = gca;
-         case 'semilogx'
-            h.fdc = semilogx(100.*f,x); ax = gca;
-         case 'linear'
-            h.fdc = plot(100.*f,x); ax = gca;
-      end
-   
-      % if requested, add a refpoint line
-      if ~isnan(refpoints)
-         hold on;
-         for n = 1:numel(refpoints)
-            xplot    = [min(xlim) 100*fref(n) 100*fref(n) 100*fref(n)];
-            yplot    = [xref(n) xref(n) min(ylim) xref(n)];
-            h.ref(n) = plot(xplot,yplot,'Color',[0.85 0.325 0.098],'LineWidth',1);
-         end
-      end
+   ylabel(['$x$ [' units ']']);
+   xlabel 'flow exceedence probability, $P(Q\ge x)$'
 
-      ylabel(['$x$ [' units ']']);
-      xlabel 'flow exceedence probability, $P(Q\ge x)$'
+   ax.YAxis.TickLabels = compose('%g',ax.YAxis.TickValues);
+   ax.XAxis.TickLabels = compose('$%g\\%%$',ax.XAxis.TickValues);
 
-      ax.YAxis.TickLabels = compose('%g',ax.YAxis.TickValues);
-      ax.XAxis.TickLabels = compose('$%g\\%%$',ax.XAxis.TickValues);
+   % since i manually set the ticklabels, i think this is necessary
+   % otherwise if the figure is resized, matlab will make new ticks
+   set(gca,'XTickMode','manual','YTickMode','manual');
 
-      % since i manually set the ticklabels, i think this is necessary
-      % otherwise if the figure is resized, matlab will make new ticks
-      set(gca,'XTickMode','manual','YTickMode','manual');
+   figformat('suppliedline',h.fdc,'linelinewidth',3); 
 
-      figformat('suppliedline',h.fdc,'linelinewidth',3); 
-   
-      fdc.h    = h;
-   end
-   
-   % package output
-   fdc.f    = f;
-   fdc.x    = x;
-   fdc.xref = xref;
-   fdc.fref = fref;
-   
+   fdc.h    = h;
+end
+
+% package output
+fdc.f    = f;
+fdc.x    = x;
+fdc.xref = xref;
+fdc.fref = fref;
+
 %    % this would also work, adapted from num2ltext
 %    ax.XAxis.TickLabels = cellstr(num2str(ax.XAxis.TickValues','$%g\\%%$'))
    
@@ -81,7 +83,7 @@ function fdc = fdcurve(flow,varargin)
 %    xtickformat 'percentage'
 %    % but they disappear when latex is set
 
-end
+
 
 % function [F,x] = ecdfpot(x,xmin,alpha,sigma)
 %    
@@ -104,5 +106,4 @@ end
 %    gamma = b-1; % might be 1-b;
 %    beta  = tau0;
 %    F     = N/n.*(1+gamma.*(x-x0)/beta)^(-1/gamm);
-%    
-% end
+
