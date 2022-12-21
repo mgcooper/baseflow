6592fcdb544ff4b78219ae0bc5fd221c8f9d7c52
mgcooper <mguycooper@gmail.com>
2022-12-21 15:58:45 -0800

    eventtau added new aggfunc options and fixed K struct dot indexing

--- +bfra/eventtau.m
+++ +bfra/eventtau.m
@@ -1,4 +1,5 @@
-function [tau,q,dqdt,tags,t,L,s,dq] = eventtau(K,Events,Fits,varargin)
+% function [tau,q,dqdt,tags,t,L,s,dq] = eventtau(K,Events,Fits,varargin)
+function [tau,q,dqdt,tags,aggvals] = eventtau(K,Events,Fits,varargin)
 %EVENTTAU compute drainage timescale tau from event-scale parameters a and b 
 % 
 %  Syntax
@@ -30,64 +31,90 @@ aggfunc = p.Results.aggfunc;
 % TODO: implement aggfunc option to compute an event-aggregate tau e.g. using
 % the mean flow, median flow, max flow, or min flow
 
-   dqfnc    = @(a,dqdt) -dqdt./a;   % must have derived this at some point
-   Taufnc   = bfra.taufunc;
-   Sfnc     = @(a,b,q) (q.^(2-b))./(a*(2-b));
-   numfits  = numel(K);            % use K b/c some 'Events' don't get fit
-   
-   Ktags    = [K.eventTag];
-   a        = [K.a];
-   b        = [K.b];
-   
-   if usefits == true
-      Q     = Fits.q;
-      dQdt  = Fits.dqdt;
-      Qtags = Fits.eventTag;
-   else
-      Q     = Events.q;
-      dQdt  = Fits.dqdt;
-      Qtags = Events.tag;
-      
-   end
-   
-   tau      = nan(size(Q));
-   q        = nan(size(Q));
-   dqdt     = nan(size(Q));
-   t        = nan(size(Q));
-   s        = nan(size(Q));
-   dq       = nan(size(Q));
-   L        = nan(numfits,1);
-   
-   for m = 1:numfits
-      tag      = Ktags(m);
-      i        = Ktags == tag;   % should just be m
-      ii       = Qtags == tag;   % Fits.eventTag == m;
-      tau(ii)  = Taufnc(a(i),b(i),Q(ii));
-      
+dqfnc    = @(a,dqdt) -dqdt./a;   % must have derived this at some point
+Taufnc   = bfra.taufunc;
+Sfnc     = @(a,b,q) (q.^(2-b))./(a*(2-b));
+
+Ktags    = K.eventTag;
+a        = K.a;
+b        = K.b;
+
+numfits  = numel(a);            % use K b/c some 'Events' don't get fit
+
+if usefits == true
+   Q     = Fits.q;
+   dQdt  = Fits.dqdt;
+   Qtags = Fits.eventTag;
+else
+   Q     = Events.q;
+   dQdt  = Fits.dqdt;
+   Qtags = Events.tag;
+
+end
+
+tau      = nan(size(Q));
+q        = nan(size(Q));
+dqdt     = nan(size(Q));
+t        = nan(size(Q));
+s        = nan(size(Q));
+dq       = nan(size(Q));
+L        = nan(numfits,1);
+
+% 
+qagg     = nan(numfits,1);
+dqagg    = nan(numfits,1);
+tauagg   = nan(numfits,1);
+
+for m = 1:numfits
+   tag      = Ktags(m);
+   i        = Ktags == tag;   % should just be m
+   ii       = Qtags == tag;   % Fits.eventTag == m;
+   tau(ii)  = Taufnc(a(i),b(i),Q(ii));
+
 % return fit q/dqdt for point cloud plot but use event q for tau
-      iii       = Fits.eventTag == tag;
-      q(iii)    = Fits.q(iii);
-      dqdt(iii) = Fits.dqdt(iii);
-      
-      
-      t(ii)    = 1:numel(Q(ii)); % should just be 1:sum(ii)
-      s(ii)    = abs(tau(ii)./(2-b(i)).*Q(ii));
-      dq(ii)   = dqfnc(a(i),dQdt(ii));
-      L(m)     = sum(ii); % L is the length of each event
-      
+   iii       = Fits.eventTag == tag;
+   q(iii)    = Fits.q(iii);
+   dqdt(iii) = Fits.dqdt(iii);
+
+
+   t(ii)    = 1:numel(Q(ii)); % should just be 1:sum(ii)
+   s(ii)    = abs(tau(ii)./(2-b(i)).*Q(ii));
+   dq(ii)   = dqfnc(a(i),dQdt(ii));
+   L(m)     = sum(ii); % L is the length of each event
+
+   % aggfunc 
+   switch aggfunc
+      case 'none'
+      case 'min'
+         qagg(m)  = min(q(iii),[],'omitnan');
+         dqagg(m) = min(dqdt(iii),[],'omitnan');
+         tauagg(m) = min(tau(ii),[],'omitnan');
+      case 'max'
+         qagg(m)  = max(q(iii),[],'omitnan');
+         dqagg(m) = max(dqdt(iii),[],'omitnan');
+         tauagg(m) = max(tau(ii),[],'omitnan');
+      case 'median'
+         qagg(m)  = median(q(iii),'omitnan');
+         dqagg(m) = median(dqdt(iii),'omitnan');
+         tauagg(m) = median(tau(ii),'omitnan');
+      case 'mean'
+         qagg(m)  = mean(q(iii),'omitnan');
+         dqagg(m) = mean(dqdt(iii),'omitnan');
+         tauagg(m) = mean(tau(ii),'omitnan');
    end
    
-   inan     = isnan(tau);
-   tau      = tau(~inan);
-   q        = q(~inan);
-   dqdt     = dqdt(~inan);
-   s        = s(~inan);
-   t        = t(~inan);
-   dq       = dq(~inan);
-   
-   tags     = Events.tag(~inan);
-   
-   
-      
-   
-   
+end
+
+inan     = isnan(tau);
+tau      = tau(~inan);
+q        = q(~inan);
+dqdt     = dqdt(~inan);
+s        = s(~inan);
+t        = t(~inan);
+dq       = dq(~inan);
+tags     = Qtags(~inan);
+
+aggvals.q = qagg;
+aggvals.dqdt = dqagg;
+aggvals.tau = tauagg;
+
