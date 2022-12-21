e1d75273a74c20e96377942da60f04ae1529f448
mgcooper <mguycooper@gmail.com>
2022-12-21 15:58:46 -0800

    robust arrow placement method for log axes

--- +bfra/plplotb.m
+++ +bfra/plplotb.m
@@ -46,99 +46,118 @@ ax          = p.Results.ax;
 %-------------------------------------------------------------------------------
    
 % get the complementary cumulative distribution function
-   numData     =  numel(x);
-   xccp        =  sort(x);
-   ccp         =  (numData:-1:1)'./numData;
-   refy        =  ccp(find(xccp>=xmin,1,'first'));
-   
-   % option to extend the line forward a bit (purely aesthetic)
-   if trimline == true
-      xccfit   =  sort(x(x>=xmin));
-      xccfit   =  [xccfit; xccfit(end)*1.2];
-   else
-      xccfit   =  sort(x); % this extends the line back toward the origin
-      xccfit   =  [xccfit; xccfit(end)*1.2]; % this extends the line forward a bit
-   end
+numData     =  numel(x);
+xccp        =  sort(x);
+ccp         =  (numData:-1:1)'./numData;
+refy        =  ccp(find(xccp>=xmin,1,'first'));
 
-   % compute the ccdf
-   ccfit       =  (xccfit./xmin).^(1-alpha);
-   
-   % scale the fitted x>xmin ccdf to pass through the refPoint
-   ccfit       =  ccfit.*refy; 
-   xccfit      =  xccfit(ccfit<=1);
-   ccfit       =  ccfit(ccfit<=1);
-   
-   % plot it
-   h.data      =  loglog(ax,xccp,ccp,'o','LineWidth',0.5,               ...
-                     'MarkerSize',10,'MarkerFaceColor','w'); hold on;
-   h.fit       =  loglog(ax,xccfit,ccfit,'LineWidth',3);   
-
-   % build a legend, labels, etc.
-   ltxt1    = ['$' varsym '$ (data)'];
-   if ~isnan(alphaci)
-      b     = bfra.conversions(alpha,'alpha','b');
-      bL    = bfra.conversions(alphaci(1),'alpha','b');
-      bH    = bfra.conversions(alphaci(2),'alpha','b');
-      ltxt2 = sprintf('MLE fit ($\\hat{b}=%.2f\\ [$%.2f,%.2f$]\\ 95\\%%$ CI)',b,bL,bH);
-   else
-      ltxt2 = sprintf('MLE fit ($b=%.2f$)',bfra.conversions(alpha,'alpha','b'));
-   end
-   xlabel('$x$');
-   ylabel(['$p(' varsym '\ge x)$'],'Interpreter','latex'); 
-   h.legend = legend(ltxt1,ltxt2,'interpreter','latex','location','southeast');
-   
-   figformat('suppliedline',h.fit,'linelinewidth',3,'labelfontsize',14);
-   
-   if ~isnan(xminci)
-      xminL = xminci(1);
-      xminH = xminci(2);
-   else
-      xminL = xmin;
-      xminH = xmin;
-   end
-   
-   if labelplot == true
-      addlabels(xccfit,ccfit,xmin,xminL,xminH,b,bL,bH,refy);
-   end
-
-   h.ax = gca;
-   h.ax.YLim(2)  = 1.5; % add a little white space above P=1
-
+% option to extend the line forward a bit (purely aesthetic)
+if trimline == true
+   xccfit   =  sort(x(x>=xmin));
+   xccfit   =  [xccfit; xccfit(end)*1.2];
+else
+   xccfit   =  sort(x); % this extends the line back toward the origin
+   xccfit   =  [xccfit; xccfit(end)*1.2]; % this extends the line forward a bit
 end
 
-function addlabels(xfit,yfit,tau0,tau0L,tau0H,b,bL,bH,yref)
-   
-   % tau0
-   xminc = (tau0H-tau0+tau0-tau0L)/2;
-   xa    = [tau0-tau0/2 tau0];
-   ya    = yfit(find(xfit>=xa(1),1,'first'));
-   ya    = [ya ya];
-   ta    = sprintf('$\\hat{\\tau}_0=%.0f\\pm%.0f$ days',tau0,xminc);
+% compute the ccdf
+ccfit       =  (xccfit./xmin).^(1-alpha);
 
-   arrow([xa(1),ya(1)],[xa(2),ya(2)],'BaseAngle',90,'Length',8,'TipAngle',10)
-   text(0.95*xa(1),ya(1),ta,'HorizontalAlignment','right','FontSize',14)
-   
-   % use these to put the text on the right side of the curve
-   %    xa    = [xmin+xmin/2 xmin];
-   %    text(1.05*xa(1),ya(1),ta)
-   
-   % deactivate this and reactive the two lines below to go back
-   % test using the new xminL/H
-   xexp = tau0*(2-b)/(3-2*b);
-   tauL = tau0L*(2-b)/(3-2*b);
-   tauH = tau0H*(2-b)/(3-2*b);
-   xexpc = (tauH-tauL)/2;
-   
-%    % tauExp
-%    xexp  = mean((tau0*(2-bL)/(3-2*bL) + tau0*(2-bH)/(3-2*bH))/2);
-%    xexpc = mean(abs(xexp-[tau0*(2-bL)/(3-2*bL), tau0*(2-bH)/(3-2*bH)]));
-   xa    = [xexp-xexp/2 xexp];
-   
-   ya    = yfit(find(xfit>=xa(2),1,'first'));
-   ya    = [ya ya];
-   ta    = sprintf('$\\langle\\tau\\rangle=%.0f\\pm%.0f$ days',xexp,xexpc);
+% scale the fitted x>xmin ccdf to pass through the refPoint
+ccfit       =  ccfit.*refy; 
+xccfit      =  xccfit(ccfit<=1);
+ccfit       =  ccfit(ccfit<=1);
 
-   arrow([xa(1),ya(1)],[xa(2),ya(2)],'BaseAngle',90,'Length',8,'TipAngle',10)
-   text(0.95*xa(1),ya(1),ta,'HorizontalAlignment','right','FontSize',14)
+% plot it
+h.data = loglog(ax,xccp,ccp,'o','LineWidth',0.5,'MarkerSize',10, ...
+   'MarkerFaceColor','w'); hold on;
+h.fit = loglog(ax,xccfit,ccfit,'LineWidth',3);
 
+% compute b for the legend
+b = bfra.conversions(alpha,'alpha','b');
+
+% build a legend, labels, etc.
+ltxt1    = ['$' varsym '$ (data)'];
+if ~isnan(alphaci)
+   bL    = bfra.conversions(alphaci(1),'alpha','b');
+   bH    = bfra.conversions(alphaci(2),'alpha','b');
+   ltxt2 = sprintf('MLE fit ($\\hat{b}=%.2f\\ [$%.2f,%.2f$]\\ 95\\%%$ CI)',b,bL,bH);
+else
+   ltxt2 = sprintf('MLE fit ($b=%.2f$)',bfra.conversions(alpha,'alpha','b'));
+   bL = nan;
+   bH = nan;
 end
+xlabel('$x$');
+ylabel(['$p(' varsym '\ge x)$'],'Interpreter','latex'); 
+h.legend = legend(ltxt1,ltxt2,'interpreter','latex','location','sw');
+
+% figformat('suppliedline',h.fit,'linelinewidth',3,'labelfontsize',14,...
+%    'legendlocation','southwest');
+
+% h.legend.Position = [0.27 0.40 0.30 0.10];
+
+if ~isnan(xminci)
+   xminL = xminci(1);
+   xminH = xminci(2);
+else
+   xminL = xmin;
+   xminH = xmin;
+end
+
+if labelplot == true
+   %addlabels(xccfit,ccfit,xmin,xminL,xminH,b,bL,bH,refy);
+   addlabels(xccfit,ccfit,xmin,xminL,xminH,b);
+end
+
+h.ax = gca;
+h.ax.YLim(2)  = 1.5; % add a little white space above P=1
+
+
+function addlabels(xfit,yfit,tau0,tau0L,tau0H,b)
+
+% tau0
+xminc = (tau0H-tau0+tau0-tau0L)/2;
+xlims = xlim;
+ndecx = log10(xlims(2))-log10(xlims(1));
+xa    = [10^(log10(tau0)-ndecx/10) tau0];
+
+ya    = yfit(find(xfit>=xa(1),1,'first'));
+ya    = [ya ya];
+
+% build the string with plus/minus if tau0L/H is provided
+if tau0==tau0L && tau0==tau0H
+   ta = sprintf('$\\hat{\\tau}_0=%.0f$ days',tau0);
+else
+   ta = sprintf('$\\hat{\\tau}_0=%.0f\\pm%.0f$ days',tau0,xminc);
+end
+
+arrow([xa(1),ya(1)],[xa(2),ya(2)],'BaseAngle',90,'Length',8,'TipAngle',10)
+text(0.95*xa(1),ya(1),ta,'HorizontalAlignment','right','FontSize',14)
+
+
+% % tauExp
+%----------------------------------------------------------------------------
+% test using the new xminL/H
+% deactivate this and reactive the two lines below to go back 
+xexp = tau0*(2-b)/(3-2*b);
+tauL = tau0L*(2-b)/(3-2*b);
+tauH = tau0H*(2-b)/(3-2*b);
+xexpc = (tauH-tauL)/2;
+% xexp  = mean((tau0*(2-bL)/(3-2*bL) + tau0*(2-bH)/(3-2*bH))/2);
+% xexpc = mean(abs(xexp-[tau0*(2-bL)/(3-2*bL), tau0*(2-bH)/(3-2*bH)]));
+%----------------------------------------------------------------------------
+
+xa    = [10^(log10(xexp)-ndecx/10) xexp];
+ya    = yfit(find(xfit>=xa(2),1,'first'));
+ya    = [ya ya];
+
+% build the string with plus/minus if tau0L/H is provided
+if tau0==tau0L && tau0==tau0H
+   ta = sprintf('$\\langle\\tau\\rangle=%.0f$ days',xexp);
+else
+   ta = sprintf('$\\langle\\tau\\rangle=%.0f\\pm%.0f$ days',xexp,xexpc);
+end
+
+arrow([xa(1),ya(1)],[xa(2),ya(2)],'BaseAngle',90,'Length',8,'TipAngle',10)
+text(0.95*xa(1),ya(1),ta,'HorizontalAlignment','right','FontSize',14)
+
