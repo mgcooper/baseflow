cc1cba137b8c59f0509db791f93882d8510f6be7
mgcooper <mguycooper@gmail.com>
2022-12-21 15:58:45 -0800

    changed output variable name to basin instead of basinname to avoid conflict with function name

--- +bfra/basinname.m
+++ +bfra/basinname.m
@@ -1,12 +1,12 @@
-function basinname = basinname(typenamehere)
-% BASINNAME return string 'basinname' from the bfra basin database
+function basin = basinname(typenamehere)
+% BASINNAME return string 'basin' from the bfra basin database
 % 
 %  Syntax
 %  
-%  basinname = bfra.basinname(<tab complete basin name>), returns the basin name
+%  basin = bfra.basinname(<tab complete basin name>), returns the basin name
 %  string as it exists in the bfra basin database
 % 
-%  basinname = bfra.basinname('ALL_BASINS'), returns string all 'ALL_BASINS'
+%  basin = bfra.basinname('ALL_BASINS'), returns string all 'ALL_BASINS'
 %  which can be passed into other functions that require the 'basinname' keyword
 %  argument to return data for all basins. Note: this option does not returns
 %  all of the basinnames
@@ -22,10 +22,10 @@ function basinname = basinname(typenamehere)
 %  Todo: 'ALL_BASINS' should return all of the basin names, see bfra.stationlist
 %  which appends 'ALL_BASINS' to the stationlist for use with loaddata functions
 
-   p              = inputParser;
-   p.FunctionName = 'bfra.basinname';
-   addRequired( p,'typenamehere');
-   parse(p,typenamehere);
-   basinname = p.Results.typenamehere;
+p              = inputParser;
+p.FunctionName = 'bfra.basinname';
+addRequired( p,'typenamehere');
+parse(p,typenamehere);
+basin = p.Results.typenamehere;
    
    
