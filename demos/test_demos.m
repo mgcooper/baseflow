082549cafcbe39723180ae9c49193fc477495ea1
mgcooper <mguycooper@gmail.com>
2023-09-04 19:22:26 -0700

    refactor(eventfinder): replace loop with arrayfun to simplify function.
    Move non nan segments to function.
    
    Intermediate commit before moving eventsplitter to local function of eventfinder.

--- demos/test_demos.m
+++ demos/test_demos.m
@@ -1,16 +1,21 @@
 function tests = test_demos
-    tests = functiontests(localfunctions);
+   %TEST_DEMOS Test 
+   % Note: to run this, replace:
+   % clearvars, close all, clc 
+   % with:
+   % clearvars -except testCase demoFiles 
+   tests = functiontests(localfunctions);
 end
 
 function test_rundemos(testCase)
-   
+
    % Get the list of .m demo files in the demos/mfiles folder
    demopath = bfra.privatefunction('demopath');
    cd(demopath('mfiles'))
-   
+
    demoFiles = dir(fullfile(demopath('mfiles'), '*.m'));
    demoFiles = string(fullfile({demoFiles.folder}, {demoFiles.name}).');
-   
+
    % Loop through each demo file and run it
    for file = demoFiles(:)'
       try
